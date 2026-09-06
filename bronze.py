ed30fb9f1ff56d922e87188822eb7981ce378157
bananaFish001 <raj123.rs308@gmail.com>
2026-09-06 18:09:23 +0000

    have loaded data from csv to tables. Also, updated data type for sale_details date columns as they are not provided in correct format

--- bronze.py
+++ bronze.py
@@ -48,12 +48,12 @@ _ = con.execute(
 _ = con.execute(
     """
     create table bronze.crm_sales_details(
-        sls_ord_num INT,
+        sls_ord_num NVARCHAR(50),
         sls_prd_key NVARCHAR(50),
         sls_cust_id NVARCHAR(50),
-        sls_order_dt DATE,
-        sls_ship_dt DATE,
-        sls_due_dt DATE,
+        sls_order_dt VARCHAR(8),
+        sls_ship_dt VARCHAR(8),
+        sls_due_dt VARCHAR(8),
         sls_sales INT,
         sls_quantity INT,
         sls_price INT
@@ -99,6 +99,76 @@ _ = con.execute(
     """
     COPY bronze.crm_cust_info
     FROM '/home/pinaka/projects/DataWarehouse/source_crm/cust_info.csv'
+    (HEADER, DELIMITER ',')
+    """
+)
+
+_ = con.execute(
+    """
+    TRUNCATE TABLE bronze.crm_prd_info
+    """
+)
+
+_ = con.sql(
+    """
+    COPY bronze.crm_prd_info
+    FROM '/home/pinaka/projects/DataWarehouse/source_crm/prd_info.csv'
+    (HEADER, DELIMITER ',')
+    """
+)
+
+_ = con.execute(
+    """
+    TRUNCATE TABLE bronze.crm_sales_details
+    """
+)
+
+_ = con.sql(
+    """
+    COPY bronze.crm_sales_details
+    FROM '/home/pinaka/projects/DataWarehouse/source_crm/sales_details.csv'
+    (HEADER, DELIMITER ',')
+    """
+)
+
+_ = con.execute(
+    """
+    TRUNCATE TABLE bronze.erp_cust_az12
+    """
+)
+
+_ = con.sql(
+    """
+    COPY bronze.erp_cust_az12
+    FROM '/home/pinaka/projects/DataWarehouse/source_erp/CUST_AZ12.csv'
+    (HEADER, DELIMITER ',')
+    """
+)
+
+_ = con.execute(
+    """
+    TRUNCATE TABLE bronze.erp_loc_a101
+    """
+)
+
+_ = con.sql(
+    """
+    COPY bronze.erp_loc_a101
+    FROM '/home/pinaka/projects/DataWarehouse/source_erp/LOC_A101.csv'
+    (HEADER, DELIMITER ',')
+    """
+)
+
+_ = con.execute(
+    """
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2
+    """
+)
+
+_ = con.sql(
+    """
+    COPY bronze.erp_px_cat_g1v2
+    FROM '/home/pinaka/projects/DataWarehouse/source_erp/PX_CAT_G1V2.csv'
     (HEADER, DELIMITER ',')
     """
 )
